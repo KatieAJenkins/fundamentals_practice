56637d49227026e00597c9bdbf341d625cef7d34
KatieAJenkins <kind2karma@gmail.com>
2017-02-12 14:56:44 -0700

    galvanize fundamentals lesson walkthrough

---
+++ week1/week1Practice.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// var greeting = "Hello There!";
+// document.getElementById('message').innerHTML = greeting;
+
+var skill = "HTML";
+var school = "Galvanize";
+
+var whatImLearning = "I am learning " + skill + " at " + school + "!";
+
+console.log(whatImLearning);
